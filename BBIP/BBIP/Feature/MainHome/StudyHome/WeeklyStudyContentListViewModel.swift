5bd200b4f773f7a78f964f039952523c205259b7
Choe-ju <juwon3415@gmail.com>
2025-06-30 00:57:49 +0900

    #69 모드 변경, 카드 선택 애니메이션 추가

--- BBIP/BBIP/Feature/MainHome/StudyHome/WeeklyStudyContentListViewModel.swift
+++ BBIP/BBIP/Feature/MainHome/StudyHome/WeeklyStudyContentListViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by 최주원 on 6/29/25.
 //
 
-import Foundation
+import SwiftUI
 
 class WeeklyStudyContentListViewModel: ObservableObject {
     // MARK: - Properties
@@ -35,13 +35,17 @@ class WeeklyStudyContentListViewModel: ObservableObject {
     // MARK: - Functions
     /// 수정 모드 전환
     func enterModifyMode() {
-        isModify = true
+        withAnimation(.easeInOut(duration: 0.15)) {
+            isModify = true
+        }
     }
     
     /// 수정 모드 진입
     func selectCard(at index: Int) {
         guard isModify else { return }
-        selectedIndex = index
+        withAnimation(.easeInOut(duration: 0.15)) {
+            selectedIndex = index
+        }
         textForEditing = modifiedContent[index]
         isEditSheetPresented = true
     }
@@ -52,19 +56,25 @@ class WeeklyStudyContentListViewModel: ObservableObject {
         }
         // Sheet를 닫고 선택 상태를 초기화
         isEditSheetPresented = false
-        selectedIndex = nil
+        withAnimation(.easeInOut(duration: 0.15)) {
+            selectedIndex = nil
+        }
         textForEditing = ""
     }
     
     /// 수정된 내용 저장
     func saveChanges() {
         // 저장 코드 추가
-        isModify = false
+        withAnimation(.easeInOut(duration: 0.15)) {
+            isModify = false
+        }
     }
     
     /// 수정 취소
     func cancelChanges() {
         modifiedContent = originalContent
-        isModify = false
+        withAnimation(.easeInOut(duration: 0.15)) {
+            isModify = false
+        }
     }
 }
