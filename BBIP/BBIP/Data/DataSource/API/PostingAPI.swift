a8d856d1ab441768ee825efe5035de8bcd8695a8
Choe-ju <juwon3415@gmail.com>
2025-07-16 19:16:35 +0900

    #72 게시글, 댓글 삭제 api 추가

--- BBIP/BBIP/Data/DataSource/API/PostingAPI.swift
+++ BBIP/BBIP/Data/DataSource/API/PostingAPI.swift
@@ -15,6 +15,8 @@ enum PostingAPI {
     case createComment(postingId: Int, content: String)  // param
     case getStudyPosting(studyId: String)
     case editPosting(postingID: String, dto: CreatePostingDTO)
+    case deletePost(postId: Int)
+    case deleteComment(commentId: Int)
 }
 
 extension PostingAPI: BaseAPI {
@@ -32,6 +34,10 @@ extension PostingAPI: BaseAPI {
                 return "/post/study/\(studyId)"
             case .editPosting(let postingId, _):
                 return "/post/\(postingId)"
+            case .deletePost(let postId):
+                return "/post/\(postId)"
+            case .deleteComment(let commentId):
+                return "/post/comment/\(commentId)"
         }
     }
     
@@ -43,12 +49,14 @@ extension PostingAPI: BaseAPI {
                 return .post
             case .editPosting:
                 return .put
+            case .deletePost, .deleteComment:
+                return .delete
         }
     }
     
     var task: Moya.Task {
         switch self {
-        case .getCurrentWeekPosting, .getPostingDetail, .getStudyPosting:
+            case .getCurrentWeekPosting, .getPostingDetail, .getStudyPosting, .deletePost, .deleteComment:
             return .requestPlain
             case .createPosting(let dto), .editPosting(_, let dto):
             return .requestJSONEncodable(dto)
