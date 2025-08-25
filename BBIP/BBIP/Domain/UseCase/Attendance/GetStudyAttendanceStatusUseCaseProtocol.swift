593f24fbe533568a6f35eed9a17a2fd2191dab6a
Choe-ju <juwon3415@gmail.com>
2025-08-25 17:57:40 +0900

    #74 추가 api 관련 코드 추가

--- BBIP/BBIP/Domain/UseCase/Attendance/GetStudyAttendanceStatusUseCaseProtocol.swift
+++ BBIP/BBIP/Domain/UseCase/Attendance/GetStudyAttendanceStatusUseCaseProtocol.swift
@@ -5,4 +5,21 @@
 //  Created by 최주원 on 8/25/25.
 //
 
-import Foundation
+import Combine
+
+protocol GetStudyAttendanceStatusUseCaseProtocol {
+    func execute(studyId: String) -> AnyPublisher<AttendanceStatusVO, AttendanceError>
+}
+
+final class GetStudyAttendanceStatusUseCase: GetStudyAttendanceStatusUseCaseProtocol {
+    private let repository: AttendanceRepository
+    
+    init(repository: AttendanceRepository) {
+        self.repository = repository
+    }
+    
+    /// 현재 진행중인 출결인증이 있는지 여부 확인
+    func execute(studyId: String) -> AnyPublisher<AttendanceStatusVO, AttendanceError> {
+        repository.getStudyAttendanceStatus(studyId: studyId)
+    }
+}
