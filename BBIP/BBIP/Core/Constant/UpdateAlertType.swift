918d157c29462235d81f7b6b69cf0094b4a5d3d4
Geonwoo Lee <lgw101142@gmail.com>
2025-09-21 20:57:23 +0900

    앱 업데이트 관련 alert type 추가

---
+++ BBIP/BBIP/Core/Constant/UpdateAlertType.swift
@@ -0,0 +1,34 @@
+//
+//  UpdateAlertType.swift
+//  BBIP
+//
+//  Created by 이건우 on 9/21/25.
+//
+
+enum UpdateAlertType {
+    case forceUpdate
+    case featureUpdate
+    case minorUpdate
+    
+    var title: String {
+        switch self {
+        case .forceUpdate:
+            return "신규 기능 업데이트 알림"
+        case .featureUpdate:
+            return "기능 업데이트 알림"
+        case .minorUpdate:
+            return "업데이트 알림"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .forceUpdate:
+            return "BBIP의 새로운 기능을 이용하기 위해서는\n업데이트가 필요해요!\n최신 버전으로 업데이트 하시겠어요?"
+        case .featureUpdate:
+            return "새로운 기능이 추가되었어요!\n지금 바로 업데이트해보세요"
+        case .minorUpdate:
+            return "BBIP의 사용성이 개선되었어요!\n지금 바로 업데이트해보세요"
+        }
+    }
+}
