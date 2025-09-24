ceaf57f1aceff74dd37e40e29379f51c3fedb1d1
Geonwoo Lee <lgw101142@gmail.com>
2025-09-24 12:46:49 +0900

    print문 내부 logger로 수정
    userStateManager

--- BBIP/BBIP/Core/Util/UserStateManager.swift
+++ BBIP/BBIP/Core/Util/UserStateManager.swift
@@ -10,18 +10,22 @@ import Foundation
 final class UserStateManager {
     private let userDataSource = UserDataSource()
     
-    /// 기존 유저인지 확인 후, 아니라면 userDefault를 업데이트
+    /// 기존 유저인지 확인 후, 아니라면 userDefault를 업데이트 합니다.
     func updateIsExistingUser(completion: @escaping () -> Void) {
         if UserDefaultsManager.shared.isExistingUser() {
-            print("기존유저 이므로 체크는 스킵.")
+            
+            BBIPLogger.log("기존 유저입니다.", level: .info, category: .feature(featureName: "[UserStateManager]]"))
             completion()
             return
         }
         
         userDataSource.checkIsNewUser { result in
-            print("checking is new user...")
+            BBIPLogger.log("checking is new user...", level: .info, category: .feature(featureName: "[UserStateManager]]"))
             UserDefaultsManager.shared.setIsExistingUser(!result)
-            print(result ? "신규유저 입니다!" : "지금부터 기존유저 입니다!")
+            
+            let resultMessage: String = result ? "신규유저 입니다." : "지금부터 기존유저로 세팅됩니다."
+            BBIPLogger.log(resultMessage, level: .info, category: .feature(featureName: "[UserStateManager]]"))
+            
             completion()
         }
     }
