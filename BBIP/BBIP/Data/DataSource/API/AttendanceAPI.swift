3de791f596d7eb781ab7c4d02fe6b4d12f8388ac
Geonwoo Lee <lgw101142@gmail.com>
2025-09-25 15:38:21 +0900

    오늘 스터디인지 확인하는 api endpoint 추가
    + 주석 수정

--- BBIP/BBIP/Data/DataSource/API/AttendanceAPI.swift
+++ BBIP/BBIP/Data/DataSource/API/AttendanceAPI.swift
@@ -10,10 +10,10 @@ import Moya
 
 enum AttendanceAPI {
     case getStatus                                          // 출석 진행 중인지 여부 확인
-    case getAttendRecord(studyId: String)                   // 출석 현황 조회
-    case createCode(studyId: String, session: Int)       // 출석 코드 생성
+    case getAttendRecord(studyId: String)                   // 출석 현황 조회 (참여/불참 멤버)
+    case createCode(studyId: String, session: Int)          // 출석 코드 생성
     case enterCode(studyId: String, code: Int)              // 코드 입력
-    case getStudyStatus(studyId: String)
+    case getStudyStatus(studyId: String)                    // 특정 스터디 출석 인증 존재 여부를 확인
 }
 
 extension AttendanceAPI: BaseAPI {
